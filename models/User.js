584d12cb05c09bed3cec48e754d7526c61fe3d5c
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-03-15 16:56:51 -0700

    added 13.5

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt')
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
@@ -41,18 +41,16 @@ User.init(
   },
   {
     hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
-   
-    async beforeCreate(newUserData) {
-    newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    return newUserData;
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
       },
 
-      // set up beforeUpdate lifecycle "hook" functionality
-    async beforeUpdate(updatedUserData) {
-    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-    return updatedUserData;
-    }
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
     },
     sequelize,
     timestamps: false,
