8f70cc3f8d28f016fb8f2430545558af4f3843c6
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-03-15 18:29:24 -0700

    finished 14.1

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -10,6 +11,16 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+// adds stylesheet
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set up Handlebars.js as apps template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
